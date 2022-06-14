839ef752a0104476def5e840d05c0b4c13779ad7
Rezza Priatna <58212770+rezzaapr@users.noreply.github.com>
2022-06-13 21:15:06 -0700

    Add files via upload

---
+++ visitor.js
@@ -0,0 +1,13 @@
+var counterContainer = document.querySelector(".footer-title");
+var resetButton = document.querySelector("#reset");
+var visitCount = localStorage.getItem("page_view");
+
+// Check if page_view entry is present
+if (visitCount) {
+  visitCount = Number(visitCount) + 1;
+  localStorage.setItem("page_view", visitCount);
+} else {
+  visitCount = 1;
+  localStorage.setItem("page_view", 1);
+}
+counterContainer.innerHTML = 'Visitor This Website : '+visitCount;
